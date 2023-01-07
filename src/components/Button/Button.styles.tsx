da2fb07d6c6707dd5cc8772e322345bb5ea36277
dotjorge <jorgeluiz_dj2010@hotmail.com>
2023-01-07 15:36:15 -0300

    component: Iniciado componente Button

---
+++ src/components/Button/Button.styles.tsx
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components'
+import { colors, radius } from 'theme'
+
+const Button = styled.button`
+  position: relative;
+  font-size: 17px;
+  letter-spacing: 0;
+  text-transform: uppercase;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  color: ${colors.black};
+  padding: 6px 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  transition: color 0.2s ease;
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    right: 0;
+    background: ${colors.secondary};
+    border: 1px solid ${colors.black};
+    border-radius: ${radius[5]};
+    transform: scale(1);
+    transform-origin: center;
+    z-index: -1;
+    outline: 2px solid transparent;
+    outline-offset: 0px;
+    transition: transform 0.2s ease, outline 0.2s ease;
+  }
+
+  &:hover {
+    color: ${colors.white};
+
+    &:before {
+      transform: scale(1.1);
+      background: ${colors.black};
+    }
+  }
+
+  &:focus {
+    &:before {
+      border: 1px solid transparent;
+      outline-color: ${colors.black};
+      outline-offset: 2px;
+    }
+  }
+`
+
+export default { Button }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes } from 'react'
+import { FC } from 'types'
+import Styled from './Button.styles'
+
+interface IButton {
+  text: string
+  iconLeft?: JSX.Element
+}
+
+type DefaultButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button: FC<IButton & DefaultButtonProps> = props => {
+  const { text, iconLeft, ...rest } = props
+
+  return (
+    <Styled.Button {...rest}>
+      <span>{iconLeft}</span>
+      {text}
+    </Styled.Button>
+  )
+}
